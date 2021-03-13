f002cc29031c51091d0fd9d489a8543f22785890
Tai <bietdanhbomanh@gmail.com>
2021-03-13 15:05:14 +0700

    fix form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,24 @@
 let login = document.querySelector('.nav__item--login');
-let register = document.querySelector('.nav__item--resgister');
+let register = document.querySelector('.nav__item--register');
 let modal = document.querySelector('.modal');
 let modalOverlay = document.querySelector('.modal__overlay');
+let historySearch = document.querySelectorAll('.search-history');
+let switchForm = document.querySelectorAll('.authen-form__switch-btn');
+let registerFrom = document.querySelector('.register-form');
+let loginForm = document.querySelector('.login-form');
+
 register.onclick = function() {
-    modal.classList.add('modal--on')
-    modal.classList.remove('login--on')
+    modal.classList.add('modal--on');
+    modal.classList.remove('login--on');
+    registerFrom.style.display = 'block';
+    loginForm.style.display = 'none';
+    registerFrom.style.backgroundColor = 'pink';
+
 
 }
 
 modalOverlay.onclick = function() {
     modal.classList.remove('modal--on')
-    
     
 }
 
@@ -18,20 +26,23 @@ modalOverlay.onclick = function() {
 login.onclick = function() {
     modal.classList.add('login--on')
     modal.classList.add('modal--on')
+    registerFrom.style.display = 'none';
+    loginForm.style.display = 'block';
 }
 
-
-
-
-let a = document.querySelector('.header__search');
-let b = a.querySelector('.search-history');
-
-// let b = document.querySelector('.search-history'); //error
-
-
-
-b.onmousedown = function (params) {
-    params.preventDefault(); 
+for(ele of historySearch) {
+    ele.onmousedown = function (e) {
+        e.preventDefault();        
+    }
 }
 
+switchForm[0].onclick = function () {
+    registerFrom.style.display = 'none';
+    loginForm.style.display = 'block';
+}
+
+switchForm[1].onclick = function () {
+    registerFrom.style.display = 'block';
+    loginForm.style.display = 'none';
+}
 
